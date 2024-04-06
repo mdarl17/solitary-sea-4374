b2140a4b541f6f64e5654ea041279b5835a73476
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-05 18:55:50 -0600

    New test_data class to help test sorting feature of #member_poros facade method; New facade test for SearchFacade; coordinates external API calls, converts data hashes into ordered member poros.

---
+++ spec/facades/search_facade_spec.rb
@@ -0,0 +1,54 @@
+require "rails_helper"
+require_relative "../test_data.rb"
+
+RSpec.describe "SearchFacade", type: :facade do 
+	before(:each) do
+		@facade = SearchFacade.new
+		@um = TestData.new.unordered_members
+  end
+	describe "class methods" do 
+		describe "#all_nation_members" do 
+			it "returns ruby objects of all members of a given nation" do 
+				nation_members = @facade.all_nation_members("Fire Nation")
+				expect(nation_members).to be_an(Array)
+				expect(nation_members.first).to be_a(Member)
+				expect(nation_members.count).to eq(97)
+				expect(nation_members.first.affiliations).to eq("Fire Nation")
+				expect(nation_members.first.allies).to eq(["Fire Nation"])
+				expect(nation_members.first.enemies).to eq(["Aang"])
+				expect(nation_members.first.name).to eq("Afiko")
+				expect(nation_members.first.photo).to eq("https://vignette.wikia.nocookie.net/avatar/images/2/24/Afiko.png/revision/latest?cb=20121121024128")
+			end
+
+			describe "#member_poros" do 
+				it "converts a parsed http response hash into ruby objects for more convenient consumption by search results view" do 
+					expect(@facade.member_poros([{
+						id: 1,
+						allies: ["Azula"],
+						enemies: ["Sokka"],
+						photoUrl: "https://airbender_photos.com",
+						name: "Bully Guard",
+						affiliation: "Fire Nation",
+					}]).first).to be_a(Member)
+				end
+
+				it "sorts the poros by member name ascending" do
+					unordered = @um
+					ordered = @facade.member_poros(@um)
+					
+					expect(unordered[0][:name]).to eq("Guard captain")
+					expect(unordered[1][:name]).to eq("Fire Nation company leader")
+					expect(unordered[2][:name]).to eq("Tom-Tom")
+					expect(unordered[3][:name]).to eq("Kori Morishita")
+					expect(unordered[4][:name]).to eq("Bully guard")
+
+					expect(ordered[0].name).to eq("Bully guard")
+					expect(ordered[1].name).to eq("Fire Nation company leader")
+					expect(ordered[2].name).to eq("Guard captain")
+					expect(ordered[3].name).to eq("Kori Morishita")
+					expect(ordered[4].name).to eq("Tom-Tom")
+				end
+			end
+		end
+	end 
+end 
---
+++ spec/test_data.rb
@@ -0,0 +1,45 @@
+class TestData
+	attr_reader :unordered_items
+
+	def unordered_members
+		[
+			{	:_id=>"5cf5679a915ecad153ab694c",
+				:allies=>["Fire Nation"],
+				:enemies=>["Aang"],
+				:photoUrl=>"https://vignette.wikia.nocookie.net/avatar/images/e/ea/Guard_captain.png/revision/latest?cb=20140116105725",
+				:name=>"Guard captain",
+				:affiliation=>"Fire Nation"
+			},
+			{:_id=>"5cf5679a915ecad153ab6926",
+				:allies=>["Sokka"],
+				:enemies=>["Earth Kingdom"],
+				:photoUrl=>
+				"https://vignette.wikia.nocookie.net/avatar/images/8/82/Fire_Nation_company_leader.png/revision/latest?cb=20121230121700",
+				:name=>"Fire Nation company leader",
+				:affiliation=>"Fire Nation Army"
+			},
+			{	:_id=>"5cf5679a915ecad153ab6a6d",
+				:allies=>["Ukano"],
+				:enemies=>[],
+				:photoUrl=>
+				"https://vignette.wikia.nocookie.net/avatar/images/4/4a/Tom-Tom_happy.png/revision/latest?cb=20130706151055",
+				:name=>"Tom-Tom",
+				:affiliation=>"Fire Nation"
+			},
+			{	:_id=>"5cf5679a915ecad153ab69a0",
+				:allies=>["Mayor "],
+				:enemies=>["Fire Lord Zuko "],
+				:photoUrl=>"https://vignette.wikia.nocookie.net/avatar/images/8/85/Kori_Morishita.png/revision/latest?cb=20160330212637",
+				:name=>"Kori Morishita",
+				:affiliation=>"Fire Nation Fire Nation colonies Yu Dao Yu Dao Resistance (formerly)"
+			},
+			{	:_id=>"5cf5679a915ecad153ab68ef",
+				:allies=>["Azula"],
+				:enemies=>["Sokka"],
+				:photoUrl=>"https://vignette.wikia.nocookie.net/avatar/images/0/02/Bully_guard.png/revision/latest?cb=20120702232626",
+				:name=>"Bully guard",
+				:affiliation=>"Fire Nation"
+			},
+		]
+	end
+end
